2dcd6e2e737551fb16ac96eed47ea0721000da0e
balajiprabhu-github <balajiprabhu008@gmail.com>
2023-05-21 21:46:32 +0530

    easy - problem 69 - square root

---
+++ src/main/kotlin/easy/SquareRoot69.kt
@@ -0,0 +1,34 @@
+package easy
+
+fun main() {
+
+    Solution().mySqrt(16)
+}
+
+class Solution {
+
+    fun mySqrt(x: Int): Int {
+
+        var start : Long = 0
+        var end : Long = x.toLong()
+        var result : Long = 0
+
+        while(start<=end) {
+
+            val mid : Long = start+((end-start)/2)
+
+            val sqr : Long = mid*mid
+
+            if(sqr > x.toLong()) {
+                end = mid-1
+            }else if(sqr < x) {
+                result = Math.max(result,mid)
+                start = mid+1
+            } else {
+                return mid.toInt()
+            }
+        }
+
+        return result.toInt()
+    }
+}
