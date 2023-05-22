29d648d24b8304377023c34bdb490337720e4b6a
balajiprabhu-github <balajiprabhu008@gmail.com>
2023-05-22 23:12:28 +0530

    easy - problem 9 - palindrome number

---
+++ src/main/kotlin/easy/PalindromeNumber9.kt
@@ -0,0 +1,26 @@
+package easy
+
+fun main() {
+
+    println( Solution9().isPalindrome(-1))
+}
+
+ class Solution9 {
+
+     fun isPalindrome(x: Int): Boolean {
+         var rev : Int = 0
+         var temp : Int = x
+
+         while (temp > 0){
+             val digit = temp%10
+             rev = (rev * 10) + digit
+             temp /= 10
+         }
+
+         if (rev == x){
+             return true
+         }
+
+         return false
+    }
+}
