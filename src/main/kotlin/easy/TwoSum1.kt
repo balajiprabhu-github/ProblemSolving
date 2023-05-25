19bb95a8a95a22543224dcfd9fa3e17db11729f8
balajiprabhu-github <balajiprabhu008@gmail.com>
2023-05-25 16:00:31 +0530

    easy - problem 1 - two sum

---
+++ src/main/kotlin/easy/TwoSum1.kt
@@ -0,0 +1,22 @@
+package easy
+
+fun main() {
+    println(Solution1().twoSum(intArrayOf(2,7,11,15),9))
+}
+
+internal class Solution1 {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val res = IntArray(2)
+        val map: MutableMap<Int, Int> = HashMap()
+        for (i in nums.indices) {
+            if (map.containsKey(target - nums[i])) {
+                res[1] = i
+                res[0] = map[target - nums[i]]!!
+                return res
+            }
+            map[nums[i]] = i
+        }
+        return res
+    }
+}
+
