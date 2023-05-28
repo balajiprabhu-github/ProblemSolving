16a294fe2a4cf906feb12baff1158f209da75ba0
balajiprabhu-github <balajiprabhu008@gmail.com>
2023-05-28 21:43:56 +0530

    easy - problem 66 - PlusOne

---
+++ src/main/kotlin/easy/PlusOne66.kt
@@ -0,0 +1,27 @@
+package easy
+
+fun main() {
+    println(Solution66().plusOne(intArrayOf(1,2,3)).toList().toString())
+    println(Solution66().plusOne(intArrayOf(4,3,2,1)).toList().toString())
+    println(Solution66().plusOne(intArrayOf(9,9,9,9,9)).toList().toString())
+    println(Solution66().plusOne(intArrayOf(7,8,2,5,9)).toList().toString())
+}
+
+class Solution66 {
+
+    fun plusOne(digits: IntArray): IntArray {
+
+        for ((i,digit) in digits.withIndex().reversed()) {
+            if (digit < 9) {
+                digits[i] += 1
+                return digits
+            } else {
+                digits[i] = 0
+            }
+        }
+
+        return IntArray(digits.size + 1).apply {
+            this[0] = 1
+        }
+    }
+}
