5e6d4b489b80e2673db9848a02549cc1881c72b5
rbalajiprabhu <balajiprabhu008@gmail.com>
2023-06-28 23:16:32 +0530

    easy - problem 1684 - CountTheNumberOfConsistentStrings

---
+++ src/main/kotlin/easy/CountTheNumberOfConsistentStrings1684.kt
@@ -0,0 +1,28 @@
+package easy
+
+fun main() {
+    println(Solution1684().countConsistentStrings("ab", arrayOf("ad","bd","aaab","baa","badab")))
+    println(Solution1684().countConsistentStrings("abc", arrayOf("a","b","c","ab","ac","bc","abc")))
+    println(Solution1684().countConsistentStrings("cad", arrayOf("a","b","c","ab","ac","bc","abc")))
+}
+
+private class Solution1684 {
+    fun countConsistentStrings(allowed: String, words: Array<String>): Int {
+        var count = 0
+
+        for (word in words) {
+            var temp = 0
+            for (c in word.toCharArray()) {
+                if (allowed.contains(c)) {
+                    temp++
+                }
+            }
+
+            if (temp == word.length) {
+                count++
+            }
+        }
+
+        return count
+    }
+}
