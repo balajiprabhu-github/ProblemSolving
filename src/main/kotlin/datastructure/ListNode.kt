bd3dae22c564c6cbfc36a715f032b91719323d3f
rbalajiprabhu <balajiprabhu008@gmail.com>
2023-07-08 23:08:15 +0530

    include ListNode operation

--- src/main/kotlin/datastructure/ListNode.kt
+++ src/main/kotlin/datastructure/ListNode.kt
@@ -3,3 +3,42 @@ package datastructure
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
+
+fun insertNode(head: ListNode?, `val`: Int): ListNode {
+    val newNode = ListNode(`val`)
+    if (head == null) {
+        return newNode
+    }
+    var current = head
+    while (current?.next != null) {
+        current = current.next
+    }
+    current?.next = newNode
+    return head
+}
+
+fun deleteNode(head: ListNode?, `val`: Int): ListNode? {
+    if (head == null) {
+        return null
+    }
+    if (head.`val` == `val`) {
+        return head.next
+    }
+    var current = head
+    var prev: ListNode? = null
+    while (current != null && current.`val` != `val`) {
+        prev = current
+        current = current.next
+    }
+    prev?.next = current?.next
+    return head
+}
+
+fun printLinkedList(head: ListNode?) {
+    var current = head
+    while (current != null) {
+        print("${current.`val`} ")
+        current = current.next
+    }
+    println()
+}
