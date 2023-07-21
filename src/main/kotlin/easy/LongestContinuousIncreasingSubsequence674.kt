6b5447083703a3e422e1e81fa85428d3b5104e3a
rbalajiprabhu <balajiprabhu008@gmail.com>
2023-07-21 20:07:11 +0530

    easy - problem 674 - LongestContinuousIncreasingSubsequence

---
+++ src/main/kotlin/easy/LongestContinuousIncreasingSubsequence674.kt
@@ -0,0 +1,27 @@
+package easy
+
+fun main() {
+    println(Solution674().findLengthOfLCIS(intArrayOf(1,3,5,4,7)))
+    println(Solution674().findLengthOfLCIS(intArrayOf(2,2,2,2,2)))
+}
+
+private class Solution674 {
+    fun findLengthOfLCIS(nums: IntArray): Int {
+
+        var result = 1
+        var prevMax = 1
+
+        for(i in 1 until nums.size) {
+            if(nums[i] > nums[i-1]) {
+                result++
+            } else {
+                result = 1
+            }
+
+            prevMax = Math.max(result,prevMax)
+
+        }
+
+        return prevMax
+    }
+}
