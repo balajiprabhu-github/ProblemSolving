40915d79c4f6edce899ecf29f9fd72f0712ff3ba
rbalajiprabhu <balajiprabhu008@gmail.com>
2023-07-23 23:54:59 +0530

    easy - problem 1337 - TheKWeakestRowsInMatrix

---
+++ src/main/kotlin/easy/TheKWeakestRowsInMatrix1337.kt
@@ -0,0 +1,39 @@
+package easy
+
+fun main() {
+    println(Solution1337().kWeakestRows(
+        arrayOf(
+            intArrayOf(1,1,0,0,0),
+            intArrayOf(1,1,1,1,0),
+            intArrayOf(1,0,0,0,0),
+            intArrayOf(1,1,0,0,0),
+            intArrayOf(1,1,1,1,1)
+        ),3
+    ).contentToString())
+}
+
+private class Solution1337 {
+    fun kWeakestRows(mat: Array<IntArray>, k: Int): IntArray {
+//        val map = mutableMapOf<Int,Int>()
+//
+//        for (i in mat.indices) {
+//            var count = 0
+//            for (j in mat[0].indices) {
+//                if (mat[i][j] == 1) {
+//                    count++
+//                    map[i] = count
+//                }
+//            }
+//        }
+//
+//        return  map.toList().sortedBy { (_,value) ->  value}.take(k).toMap().keys.toIntArray()
+        val rowStrengths = mat.mapIndexed { index, row ->
+            Pair(index, row.sum())
+        }
+
+        return rowStrengths.sortedWith(compareBy({ it.second }, { it.first }))
+            .take(k)
+            .map { it.first }
+            .toIntArray()
+    }
+}
