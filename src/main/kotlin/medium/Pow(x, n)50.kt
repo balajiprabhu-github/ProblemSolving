c270a388b6f1ce9a530abc16b0f083363576652f
rbalajiprabhu <balajiprabhu008@gmail.com>
2023-07-24 23:01:54 +0530

    medium - problem 50 - Pow(x, n)

---
+++ src/main/kotlin/medium/Pow(x, n)50.kt
@@ -0,0 +1,25 @@
+package medium
+
+import kotlin.math.abs
+
+fun main() {
+    println(Solution50().myPow(2.00000,10))
+}
+
+class Solution50 {
+    fun myPow(x: Double, n: Int): Double {
+
+        val num: Double = if (n < 0) 1/x else x
+        var power: Long = abs(n.toLong())
+        var product: Double = num
+        var res = 1.0
+        while (power > 0) {
+            if (power%2 == 1L) {
+                res *= product
+            }
+            power /= 2
+            product *= product
+        }
+        return res
+    }
+}
