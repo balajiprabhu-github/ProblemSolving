ec10349ca84b5158148681c0949101020cadd12a
rbalajiprabhu <balajiprabhu008@gmail.com>
2023-09-20 17:48:04 +0530

    medium - problem 2807 InsertGreatestCommonDivisorsInLinkedList

---
+++ src/main/kotlin/medium/InsertGreatestCommonDivisorsInLinkedList2807.kt
@@ -0,0 +1,36 @@
+package medium
+
+import datastructure.ListNode
+
+class Solution2807 {
+
+    fun insertGreatestCommonDivisors(head: ListNode?): ListNode? {
+
+        if(head?.next == null) {
+            return head
+        }
+
+        var current = head
+
+
+        while(current?.next != null) {
+            val a = current.`val`
+            val b = current.next?.`val`
+            val newNode = ListNode(gcd(a,b ?: 0))
+            newNode.next = current.next
+            current.next = newNode
+            current = newNode.next
+        }
+
+        return head
+
+    }
+
+    private fun gcd(a: Int, b:Int) : Int {
+        return if(b == 0) {
+            a
+        } else {
+            gcd(b,a % b)
+        }
+    }
+}
