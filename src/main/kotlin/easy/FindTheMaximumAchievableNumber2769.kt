484de29b5c46b557f84efb946151f406dd95595b
balajiprabhu-github <balajiprabhu008@gmail.com>
2023-11-05 23:42:42 +0530

    make private

--- src/main/kotlin/easy/FindTheMaximumAchievableNumber2769.kt
+++ src/main/kotlin/easy/FindTheMaximumAchievableNumber2769.kt
@@ -4,7 +4,7 @@ fun main() {
     Solution2769().theMaximumAchievableX(4,1)
 }
 
-class Solution2769 {
+private class Solution2769 {
     fun theMaximumAchievableX(num: Int, t: Int): Int {
         return num + t + t
     }
