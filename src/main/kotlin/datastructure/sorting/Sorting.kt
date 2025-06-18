b3dc50638c72e68cf1cf5dcc3802e0444ddce8cb
balajiprabhu-github <balajiprabhu008@gmail.com>
2025-06-18 10:33:52 +0530

    bubble sort

--- src/main/kotlin/datastructure/sorting/Sorting.kt
+++ src/main/kotlin/datastructure/sorting/Sorting.kt
@@ -2,8 +2,14 @@ package datastructure.sorting
 
 fun main() {
     val array = arrayOf(9,5,1,4,8,2,7,10,3)
-    SelectionSort().sort(array)
+    BubbleSort().sort(array)
     println(array.contentToString())
+}
+
+private fun swap(array: Array<Int>, indexA: Int, indexB: Int) {
+    val temp = array[indexA]
+    array[indexA] = array[indexB]
+    array[indexB] = temp
 }
 
 class SelectionSort {
@@ -18,11 +24,17 @@ class SelectionSort {
             swap(array, i, min)
         }
     }
+}
 
-    private fun swap(array: Array<Int>, indexA: Int, indexB: Int) {
-        val temp = array[indexA]
-        array[indexA] = array[indexB]
-        array[indexB] = temp
+class BubbleSort {
+    fun sort(array: Array<Int>) {
+        for ( i in array.size-1 downTo 0) {
+            for(j in 0 until i) {
+                if (array[j] > array[j+1]) {
+                    swap(array,j,j+1)
+                }
+            }
+        }
     }
 }
 
