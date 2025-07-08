988e81e79e99b5aaa342056a49332e0d026f1d3f
balajiprabhu-github <balajiprabhu008@gmail.com>
2025-07-08 06:33:45 +0530

    merge sort

--- src/main/kotlin/datastructure/sorting/Sorting.kt
+++ src/main/kotlin/datastructure/sorting/Sorting.kt
@@ -2,7 +2,7 @@ package datastructure.sorting
 
 fun main() {
     val array = arrayOf(9,5,1,4,8,2,7,10,3)
-    InsertionSort().sort(array)
+    MergeSort().sort(array)
     println(array.contentToString())
 }
 
@@ -46,6 +46,50 @@ class InsertionSort {
                 swap(array,j-1,j)
                 j--
             }
+        }
+    }
+}
+
+class MergeSort {
+    fun sort(array: Array<Int>) {
+        divideAndMerge(array,0,array.size-1)
+    }
+
+    private fun divideAndMerge(array: Array<Int>, low: Int, high: Int) {
+        if(low >= high) return
+        val mid = (low + high) / 2
+        divideAndMerge(array,low, mid)
+        divideAndMerge(array,mid+1, high)
+        merge(array,low, mid, high)
+    }
+
+    private fun merge(array: Array<Int>, low: Int, mid:Int, high: Int) {
+        var left = low
+        var right = mid+1
+        val temp = mutableListOf<Int>()
+
+        while(left <= mid && right <= high) {
+            if(array[left] <= array[right]) {
+                temp.add(array[left])
+                left++
+            } else {
+                temp.add(array[right])
+                right++
+            }
+        }
+
+        while(left <= mid) {
+            temp.add(array[left])
+            left++
+        }
+
+        while(right <= high) {
+            temp.add(array[right])
+            right++
+        }
+
+        for (i in temp.indices) {
+            array[low + i] = temp[i]
         }
     }
 }
